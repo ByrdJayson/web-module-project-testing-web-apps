6c27cd46d31a1dfc2268c3c05a5bd5a803277ea5
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-02 16:50:29 -0500

    Working on All Fields Being Entered Errors

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -13,12 +13,15 @@ test('renders without errors', ()=>{
 test('renders the contact form header', ()=> {
     render(<ContactForm/>);
     const contactHeader = screen.queryByText('Contact Form');
-    expect(contactHeader).toBeInDocument();
+    expect(contactHeader).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
   render(<ContactForm/>);
   const firstNameInput = screen.getByLabelText('First Name*');
+  userEvent.type(firstNameInput, 'JAB2');
+  const error =  await screen.findAllByTestId('error');
+  expect(error).toHaveLength(1);
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
